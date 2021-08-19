758c2e79bdf4241b2708b490561e7823cf885a00
alex v <alex@encrypt-s.com>
2021-08-19 12:51:16 +0200

    update binance

--- src/providers/connectors/bsc.ts
+++ src/providers/connectors/bsc.ts
@@ -1,16 +1,6 @@
-const ConnectToBinance = async () => {
-  let provider = null;
-  if (typeof window.BinanceChain !== 'undefined') {
-    provider = window.BinanceChain;
-    try {
-      await provider.request({ method: 'eth_requestAccounts' })
-    } catch (error) {
-      throw new Error("User Rejected");
-    }
-    provider = window.web3.currentProvider;
-  } else {
-    throw new Error("No Web3 Provider found");
-  }
+const ConnectToBinance = async (BscConnectProvider: any) => {
+  const provider = new BscConnectProvider();
+  
   return provider;
 };
 
